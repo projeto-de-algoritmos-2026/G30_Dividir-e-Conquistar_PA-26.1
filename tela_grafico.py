04004790b6206484d34b85c4d6d67c980e861d03
VituuDias <vitorgabriel30001@gmail.com>
2026-05-31 15:59:03 -0300

    chore: estrutura inicial dos módulos de tela

---
+++ tela_grafico.py
@@ -0,0 +1,6 @@
+import tkinter as tk
+
+from app import (COR_BG, COR_SURFACE, COR_CARD, COR_ACCENT, COR_ACCENT2,
+                 COR_TEXT, COR_TEXT_MUT, COR_OK, COR_WARN,
+                 FONTE_TITULO, FONTE_SUB, FONTE_CORPO, FONTE_SMALL)
+from inversions import total_inversoes_grupo, similaridade
---
+++ tela_inversoes.py
@@ -0,0 +1,7 @@
+import tkinter as tk
+from tkinter import ttk
+
+from app import (COR_BG, COR_SURFACE, COR_CARD, COR_ACCENT, COR_ACCENT2,
+                 COR_TEXT, COR_TEXT_MUT, COR_OK, COR_WARN,
+                 FONTE_TITULO, FONTE_SUB, FONTE_CORPO, FONTE_SMALL)
+from inversions import contar_inversoes, similaridade
